7b8a3f23152b2ff0ce8496ac1343b1fd41d0f96c
MoFawzy999 <mohmedfawzy988@gmail.com>
2024-12-14 02:06:25 +0200

    second-commit

--- myScript.js
+++ myScript.js
@@ -1,6 +1,6 @@
 // main variables
 let categoriesArr = ["All", "To-Do", "Completed"];
-let arrayOfTasks = [];
+let arrayOfTasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
 let completedTasks = [];
 let unCompletedTasks = [];
 let taskInput = document.querySelector(".modal .modal-body input");
@@ -11,20 +11,17 @@ let categoryTitle = document.querySelector("section .head h2");
 function renderCategories() {
   categoriesArr.forEach((category, i) => {
     document.querySelector("main aside ul").innerHTML += `
-      <li class="p-md-3 fs-md-4 fw-medium rounded-2" onclick="event.stopPropagation();selectTasksCategory(${i})">
+      <li class="p-md-3 fs-md-4 fw-medium rounded-2" onclick="event.stopPropagation();
+      selectTasksCategory(${i})">
       ${category}</li>`;
   });
 }
-renderCategories();
-selectTasksCategory(0);
 function selectTasksCategory(indexElement) {
   categoryTitle.innerText = categoriesArr[indexElement];
   document.querySelectorAll("aside ul li").forEach((li) => {
     li.classList.remove("active");
   });
-  document
-    .querySelector(`aside ul li:nth-of-type(${indexElement + 1})`)
-    .classList.add("active");
+  document.querySelector(`aside ul li:nth-of-type(${indexElement + 1})`).classList.add("active");
   if (indexElement == 0) {
     renderTasks(arrayOfTasks);
   } else if (indexElement == 1) {
@@ -38,22 +35,22 @@ function renderTasks(array) {
   array.forEach((task, i) => {
     tasksContainer.innerHTML += `
          <li class="task rounded-3 p-3 d-flex justify-content-between align-items-center mb-3">
-            <div class="content flex-grow-1">
+            <div class="content flex-md-grow-1">
                <h4 contenteditable=${task.isUpdated ? "true" : "false"}>${task.name}</h4> 
                <span>${task.date}</span>
             </div>
-            <div class="update ms-2" onclick="event.stopPropagation();updateTask(${i})">
-            <i class="fa-solid ${
-              task.isUpdated ? "fa-floppy-disk" : "fa-pencil"
-            }"></i>
-            </div>
-            <div class="delete" onclick="event.stopPropagation(); deleteTask(${i},'${task.name}')">
-            <i class="fa-regular fa-trash-can"></i></div>
-            <div class="status">
-              <input type="checkbox" id="task-${i}-status" class="rounded-circle" 
-              onchange="toggleTaskStatus(${i})"
-                ${task.completed == true ? "checked" : null}/>
-              <label for="task-${i}-status" class="toggle-check pe-4"></label>
+            <div class="options d-flex justify-content-center align-items-center gap-2">
+              <div class="update ms-2" onclick="event.stopPropagation(); updateTask(${task.id},${i})">
+                <i class="fa-solid ${task.isUpdated ? "fa-floppy-disk" : "fa-pencil"}"></i>
+              </div>
+              <div class="delete" onclick="event.stopPropagation(); deleteTask(${task.id})">
+                <i class="fa-regular fa-trash-can"></i>
+              </div>
+              <div class="status">
+                <input type="checkbox" id="task-${i}-status" class="rounded-circle" 
+                  onchange="toggleTaskStatus(${task.id})" ${task.completed == true ? "checked" : null}/>
+                <label for="task-${i}-status" class="toggle-check pe-4"></label>
+              </div>
             </div>
          </li>
       `;
@@ -71,6 +68,8 @@ function renderCompletedTasks() {
   });
   renderTasks(completedTasks);
 }
+renderCategories();
+selectTasksCategory(0);
 //===============================================================================================================
 // CRUD operations
 function createTask() {
@@ -84,96 +83,89 @@ function createTask() {
       ? "0" + (currentDate.getMonth() + 1)
       : currentDate.getMonth() + 1;
   let year = currentDate.getFullYear();
-  let hours = currentDate.getHours();
+  let hours = currentDate.getHours() >= 12 ? currentDate.getHours() -12 : currentDate.getHours() ;
   let minutes = currentDate.getMinutes();
   let seconds = currentDate.getSeconds();
+  let generateID = localStorage.getItem('tasks') ?
+       JSON.parse(localStorage.getItem('tasks')).length > 0 ? 
+        JSON.parse(localStorage.getItem('tasks'))[JSON.parse(localStorage.getItem('tasks')).length -1].id
+        : 0  : 0 ;
   let task = {
+    id :  generateID + 1  ,
     name: taskInput.value,
-    date: `${day}/${month}/${year} , ${hours >= 12 ? hours - 12 : hours}:${minutes}:${seconds} 
-               ${hours >= 12 ? "PM" : "AM"}` ,
+    date: `${day}/${month}/${year} , ${hours}:${minutes}:${seconds} 
+               ${hours >= 12 ? "PM" : "AM"}`,
     completed: false,
     isUpdated: false
   };
   arrayOfTasks.push(task);
   taskInput.value = "";
+  localStorage.setItem('tasks',JSON.stringify(arrayOfTasks));
 }
 function addTask() {
   let spaceReg = /\S/i;
   if (spaceReg.test(taskInput.value)) {
     createTask();
   }
-  renderTasks(arrayOfTasks);
-}
-function deleteTask(taskIndex,taskName) {
-  if (categoryTitle.innerText == "All") {
-      arrayOfTasks.splice(taskIndex,1);
-      renderTasks(arrayOfTasks);
-  } else if (categoryTitle.innerText == "To-Do") {
-      unCompletedTasks.splice(taskIndex,1);
-      renderTasks(unCompletedTasks);
-      arrayOfTasks = arrayOfTasks.filter((task) =>{
-         return task.name != taskName ;
-      });
-  } else {
-      completedTasks.splice(taskIndex,1); 
-      renderTasks(completedTasks);
-      arrayOfTasks = arrayOfTasks.filter((task) =>{
-         return task.name != taskName ;
-      });
+  if(categoryTitle.innerText == 'All'){
+    renderTasks(arrayOfTasks);
+  }else if(categoryTitle.innerText == 'To-Do'){
+    renderUncompletedTasks();
+  }else{
+    renderCompletedTasks();
   }
 }
-function toggleTaskStatus(taskIndex) {
-  if(categoryTitle.innerText == "All") {
-    arrayOfTasks[taskIndex].completed = !arrayOfTasks[taskIndex].completed;
-  } else if (categoryTitle.innerText == "To-Do") {
-    unCompletedTasks[taskIndex].completed = !unCompletedTasks[taskIndex].completed;
-    /*arrayOfTasks.forEach( (task) =>{
-         if(task.name == taskName){
-             task.completed = !task.completed ;
-         }
-      });*/
-    setTimeout(() => {
-      unCompletedTasks.splice(taskIndex, 1);
-      renderTasks(unCompletedTasks);
-    }, 500);
-  } else {
-    completedTasks[taskIndex].completed = !completedTasks[taskIndex].completed;
-    /*arrayOfTasks.forEach( (task) =>{
-         if(task.name == taskName){
-             task.completed = !task.completed ;
-         }
-      });*/
-    setTimeout(() => {
-      completedTasks.splice(taskIndex, 1);
-      renderTasks(completedTasks);
-    }, 500);
-  }
-}
-function updateTask(taskIndex) {
-  let updatedContent = document.querySelectorAll("section .tasks .task .content h4");
+function deleteTask(taskID) {
+  arrayOfTasks = arrayOfTasks.filter( (task) =>{
+    return task.id != taskID ;
+  });
   if (categoryTitle.innerText == "All") {
-    if (arrayOfTasks[taskIndex].isUpdated == true) {
-      arrayOfTasks[taskIndex].name = updatedContent[taskIndex].innerText.trim();
-      arrayOfTasks[taskIndex].isUpdated = false;
-    } else {
-      arrayOfTasks[taskIndex].isUpdated = true;
-    }
     renderTasks(arrayOfTasks);
   } else if (categoryTitle.innerText == "To-Do") {
-    if (unCompletedTasks[taskIndex].isUpdated == true) {
-      unCompletedTasks[taskIndex].name = updatedContent[taskIndex].innerText.trim();
-      unCompletedTasks[taskIndex].isUpdated = false;
-    } else {
-      unCompletedTasks[taskIndex].isUpdated = true;
-    }
-    renderTasks(unCompletedTasks);
+    renderUncompletedTasks();
   } else {
-    if (completedTasks[taskIndex].isUpdated == true) {
-      completedTasks[taskIndex].name = updatedContent[taskIndex].innerText.trim();
-      completedTasks[taskIndex].isUpdated = false;
-    } else {
-      completedTasks[taskIndex].isUpdated = true;
-    }
-    renderTasks(completedTasks);
+    renderCompletedTasks();
   }
+  localStorage.setItem('tasks',JSON.stringify(arrayOfTasks));
+}
+
+function toggleTaskStatus(taskID) {
+  arrayOfTasks.forEach( (task) =>{
+      if(task.id == taskID){
+        task.completed = !task.completed ;
+      }
+  });
+  if (categoryTitle.innerText == "All") {
+    renderTasks(arrayOfTasks);
+  }else if (categoryTitle.innerText == "To-Do") {
+    setTimeout(() =>{
+      renderUncompletedTasks();
+    },1000);
+  } else {
+    setTimeout(() =>{
+      renderCompletedTasks();
+    },1000);
+  }
+  localStorage.setItem('tasks',JSON.stringify(arrayOfTasks));
+}
+function updateTask(taskID,taskIndex) {
+  let updatedContent = document.querySelectorAll("section .tasks .task .content h4");
+  arrayOfTasks.forEach( (task) => {
+    if(task.id == taskID){
+      if(task.isUpdated == true) {
+          task.name = updatedContent[taskIndex].innerText.trim();
+          task.isUpdated = false ;
+      }else{
+        task.isUpdated = true ;
+      }
+    }
+  });
+  if (categoryTitle.innerText == "All") {
+    renderTasks(arrayOfTasks);
+  }else if(categoryTitle.innerText == "To-Do"){
+    renderUncompletedTasks();
+  }else{
+    renderCompletedTasks();
+  }
+  localStorage.setItem('tasks',JSON.stringify(arrayOfTasks));
 }
